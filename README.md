34b3b94277e9a20f326b97b1e4ace62d2c3c6093
Alexandre Peyroux <alex@px.io>
2019-05-31 15:39:57 +0200

    fst commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# mqtt-webhook
+
+## Exemple
+
+``` shell
+curl -H "vernemq-hook: auth_on_register" -H "Content-Type: application/json" -v -d '{"username": "alexandre.px", "client_id": "XXXXX1"}' -XPOST http://10.227.193.18/ifup
+```
+
+## web-hook podam
+
+``` shell
+ssh ubuntu@10.227.193.18
+```
+
---
+++ src/Main.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Main where
+
+import Control.Monad.Trans (liftIO)
+import Data.Aeson
+import Data.Text (Text)
+import GHC.Generics
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Logger (withStdoutLogger)
+import Servant
+import Servant
+import Servant.API
+
+data MqttClient = MqttClient {
+  mcUsername :: Text
+  , mcId :: Text
+} deriving (Generic, Show)
+
+instance ToJSON MqttClient
+instance FromJSON MqttClient where
+  parseJSON (Object v) = MqttClient
+        <$> v .: "username"
+        <*> v .: "client_id"
+
+data MqttHookResponse = MqttHookResponse {
+  mhrResult :: Text
+} deriving (Generic, Show)
+
+instance ToJSON MqttHookResponse where
+  toJSON (MqttHookResponse msg) = object ["result" .= msg]
+
+type MqttWebHook = "ifup" :> Header "vernemq-hook" Text :> ReqBody '[JSON] MqttClient :> Post '[JSON] MqttHookResponse
+
+mqttWebHook :: Proxy MqttWebHook
+mqttWebHook = Proxy
+
+whrOk :: Maybe Text -> MqttClient -> Handler MqttHookResponse
+whrOk (Just "auth_on_register") c = do
+  liftIO $ print c
+  return $ MqttHookResponse "ok"
+whrOk (Just "auth_on_subscribe") c = do
+  liftIO $ print c
+  return $ MqttHookResponse "ok"
+whrOk _ _ = return $ MqttHookResponse "next"
+
+srvMqttWebHook :: Server MqttWebHook
+srvMqttWebHook = whrOk
+
+appMqttWebHook :: Application
+appMqttWebHook = serve mqttWebHook srvMqttWebHook
+
+main :: IO ()
+main = do
+  putStrLn "starting mqtt hook listener ..."
+  withStdoutLogger $ \aplogger -> do
+        let settings = setPort 80 $ setLogger aplogger defaultSettings
+        runSettings settings appMqttWebHook

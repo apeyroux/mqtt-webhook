19b25d9b5f3e3e8eee165e91a8345aa3fda0ca33
Alexandre Peyroux <alex@px.io>
2019-07-12 09:56:41 +0200

    rm fhs

--- default.nix
+++ default.nix
@@ -3,12 +3,6 @@ with import <nixpkgs> {};
 
 let
   drv = haskellPackages.callCabal2nix "mqtt-webhook" ./. {};
-in {
-
-  nix = if lib.inNixShell then drv.env.overrideAttrs (old: {
+in if lib.inNixShell then drv.env.overrideAttrs (old: {
     buildInputs = old.buildInputs ++ [ haskellPackages.ghcid cabal-install ];
-  }) else drv;
-
-  fhs = (buildFHSUserEnv { name = "webhook"; targetPkgs = p: (with p; [ghc gcc cabal-install binutils.bintools zlib.dev]); }).env;
-
-}
+  }) else drv

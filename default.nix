75a1cd08cc178027c326386ab225392283d5f3f3
Alexandre Peyroux <alex@px.io>
2019-08-21 09:39:42 +0200

    up nix

--- default.nix
+++ default.nix
@@ -2,13 +2,20 @@ with import <nixpkgs> {};
 
 let
   version = "1.0";
+  srvProd = "10.0.0.1";
   pypi2nix = (import ./nix/requirements.nix {}).packages;
   requirements = with python3Packages; [
       flask
       click
       pypi2nix.pampy
   ];
+  vmDebian = pkgs.vmTools.diskImageFuns.debian8x86_64 {};
 in rec {
+  deploy = writeScriptBin "deploy-mqttwebhook" ''
+echo "=== DEPLOY IMG ==="
+scp ${docker} ${srvProd}:/tmp
+echo "=== LOAD IMG ==="
+  '';
   mqtt-webhook = python3Packages.buildPythonPackage {
     propagatedBuildInputs = requirements;
     name = "mqtt-webhook-${version}";

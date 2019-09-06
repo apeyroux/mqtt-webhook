42557fc532e1800d3bfdeacf01e48ad949df3ca4
Alexandre Peyroux <alex@px.io>
2019-09-06 15:14:01 +0200

    init neotoken

--- main.go
+++ main.go
@@ -18,11 +18,25 @@ type webookPayload struct {
 	CleanSession bool   `json:"clean_session"`
 }
 
-func checkAuthType(userName string) int {
+func checkAuthType(userName string, password string) int {
 	switch userName {
 	case "ident":
 		return 1
 	case "auth":
+		sPassword := strings.Split(password, ":")
+		// check si j'ai bien uid et token
+		if len(sPassword) != 2 {
+			return 0
+		}
+		// todo: caca
+		req, err := http.Get(fmt.Sprintf("http://neotoken.gendarmerie.fr/token/check/?uid=%s&token=%s", sPassword[0], sPassword[1]))
+		if err != nil {
+			log.Printf("ERROR: %s", err)
+			return 0
+		}
+		if req.Request.Response.StatusCode != 200 {
+			return 0
+		}
 		return 2
 	case "neoparc":
 		return 3
@@ -44,13 +58,13 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 		if len(sUserName) == 0 {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
-		if checkAuthType(sUserName[0]) != 0 {
+		if checkAuthType(sUserName[0], p.Password) != 0 {
 			w.Header().Set("Content-Type", "application/json")
 			fmt.Fprintf(w, "{'result': 'ok'}")
-			log.Printf("😍 id:%s user:%s password:%s", p.ClientID, p.UserName, p.Password)
+			log.Printf("ok 😍 id:%s user:%s password:%s", p.ClientID, p.UserName, p.Password)
 		} else {
 			w.WriteHeader(http.StatusForbidden)
-			log.Printf("⛔ id:%s user:%s password:%s", p.ClientID, p.UserName, p.Password)
+			log.Printf("ko ⛔ id:%s user:%s password:%s", p.ClientID, p.UserName, p.Password)
 		}
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -60,5 +74,5 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/auth", webhookHandler)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8080", nil)
 }

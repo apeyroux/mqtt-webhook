5e1bc32bf65aa0879d43cc6d1dbabeca8929a566
Alexandre Peyroux <alex@px.io>
2019-09-13 20:52:33 +0200

    init ws with actix

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,202 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(bind_by_move_pattern_guards)]
 
-#[macro_use] extern crate rocket;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate clap;
 
-use rocket::Outcome;
-use rocket::http::Status;
-use rocket::request::{self, Request, FromRequest};
-use rocket::response::content;
+use actix_web::middleware::Logger;
+use actix_web::{
+    error, middleware, web, App as WebApp, Error, HttpRequest, HttpResponse, HttpServer,
+};
+use clap::{App, Arg};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+use simplelog::*;
+use std::fs::File;
 
-fn checkIdent(i: &str) -> bool {
-    // i == "0000"
-    true
+type IMEI = String;
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+struct WebHookPayload {
+    peer_addr: String,
+    peer_port: i32,
+    username: Option<String>,
+    password: Option<String>,
+    mountpoint: String,
+    client_id: String,
+    clean_session: bool,
 }
 
-fn is_valid_auth(key: &str) -> bool {
-    let k: Vec<&str> = key.split(":").collect();
-    match &k[..] {
-        ["neoparc", _, _] => true,
-        ["ident", i, _] => checkIdent(i),
-        ["auth", _, _] => true,
-        _ => false
-    }
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+enum Authentication {
+    Token {
+        imei: IMEI,
+        idtel: String,
+        uid: String,
+        token: String,
+    },
+    Ident {
+        imei: IMEI,
+        idtel: String,
+    },
+    Login {
+        username: String,
+        password: String,
+    },
 }
 
-#[derive(Debug)]
-enum HookError {
-    BadCount,
-    Missing,
-    Invalid,
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "result")]
+#[serde(rename_all = "lowercase")]
+enum WebHookResult {
+    Ok,
+    Failed,
 }
 
-#[derive(Debug)]
-struct VernemqHook (String);
-
-impl<'a, 'r> FromRequest<'a, 'r> for VernemqHook {
-    type Error = HookError;
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        let hooks: Vec<_> = request.headers().get("vernemq-hook").collect();
-        match hooks.len() {
-            0 => Outcome::Failure((Status::BadRequest, HookError::Missing)),
-            1 if is_valid_auth(hooks[0]) => Outcome::Success(VernemqHook(hooks[0].to_string())),
-            1 => Outcome::Failure((Status::Unauthorized, HookError::Invalid)),
-            _ => Outcome::Failure((Status::Unauthorized, HookError::BadCount)),
+fn login_to_auth(username: String, password: String) -> Result<Authentication, String> {
+    // unimplemented!();
+    let vu: Vec<&str> = username.split(':').collect();
+    match *vu {
+        [] => Err(String::from("Comprend pas")),
+        [login] => Ok(Authentication::Login {
+            username: login.to_string(),
+            password: password.to_string(),
+        }),
+        [auth_type, imei, idtel] if auth_type == "ident" => Ok(Authentication::Ident {
+            imei: imei.to_string(),
+            idtel: idtel.to_string(),
+        }),
+        [auth_type, imei, idtel] if auth_type == "auth" => {
+            let vt: Vec<&str> = password.split(':').collect();
+            match *vt {
+                [uid, token] => Ok(Authentication::Token {
+                    uid: uid.to_string(),
+                    token: token.to_string(),
+                    imei: imei.to_string(),
+                    idtel: idtel.to_string(),
+                }),
+                _ => Err(String::from(format!(
+                    "this imei {} is trying to use a password {} that is not a token.",
+                    imei, password
+                ))),
+            }
         }
+        _ => Err(String::from("I don't understand the username chain.")),
     }
 }
 
-#[get("/")]
-fn auth(hook: VernemqHook) -> content::Json<&'static str> {
-    content::Json("{'result': 'ok'}")
+fn auth_is_ok(auth: &Authentication) -> WebHookResult {
+    match auth {
+        Authentication::Token { .. } => WebHookResult::Failed,
+        Authentication::Ident { .. } => WebHookResult::Ok,
+        Authentication::Login {
+            username: u,
+            password: p,
+        } if u == "88mph" && p == "88mph" => WebHookResult::Ok,
+        _ => WebHookResult::Failed,
+    }
 }
 
+fn ws_auth(client: web::Json<WebHookPayload>, req: HttpRequest) -> HttpResponse {
+    match req.headers().get("vernemq-hook") {
+        Some(hv) if hv.to_str().unwrap() == "auth_on_register" => match client.into_inner() {
+            WebHookPayload {
+                peer_addr: _,
+                peer_port: _,
+                username: Some(username),
+                password: Some(password),
+                mountpoint: _,
+                client_id: _,
+                clean_session: _,
+            } => match login_to_auth(username, password) {
+                Ok(auth) => {
+                    if auth_is_ok(&auth) == WebHookResult::Ok {
+                        info!("ok {:?}", auth);
+                        HttpResponse::Ok().json(WebHookResult::Ok)
+                    } else {
+                        info!("ko {:?}", auth);
+                        HttpResponse::Forbidden().json(WebHookResult::Failed)
+                    }
+                }
+                Err(err) => {
+                    info!("ko {}", err);
+                    HttpResponse::Forbidden().json(WebHookResult::Failed)
+                }
+            },
+            _ => HttpResponse::Forbidden().json(WebHookResult::Failed),
+        },
+        _ => HttpResponse::Forbidden().json(WebHookResult::Failed), // <- send response
+    }
+}
+
+//
+// MAIN
+//
 fn main() {
-    rocket::ignite().mount("/auth", routes![auth]).launch();
+    let matches = clap::App::new("mqtt-webhook")
+        .about("MQTT-WEBHOOK")
+        .version("0.1")
+        .arg(
+            Arg::with_name("listen")
+                .short("l")
+                .long("listen")
+                .takes_value(true)
+                .default_value("127.0.0.1:8080")
+                .env("LISTEN_WS"),
+        )
+        .arg(
+            Arg::with_name("logfile")
+                .long("log")
+                .takes_value(true)
+                .default_value("mqtt-webhook.log")
+                .env("MQTT_WEBHOO_LOG"),
+        )
+        .get_matches();
+
+    let listen = matches.value_of("listen").unwrap();
+    let logfile = matches.value_of("logfile").unwrap();
+
+    CombinedLogger::init(vec![
+        TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed).unwrap(),
+        WriteLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            File::create(logfile).unwrap(),
+        ),
+    ])
+    .unwrap();
+
+    HttpServer::new(|| {
+        WebApp::new()
+            .data(web::JsonConfig::default().limit(4096))
+            .service(web::resource("/auth").route(web::post().to(ws_auth)))
+    })
+    .bind(listen)
+    .unwrap()
+    .run();
+}
+
+//
+// TEST
+//
+#[test]
+fn test_check_auth_token() {
+    let t = login_to_auth("auth:323232:3232k".to_string(), "uid:token".to_string()).unwrap();
+    assert_eq!(WebHookResult::Failed, auth_is_ok(&t))
+}
+
+#[test]
+fn test_check_auth_login_failed() {
+    let t = login_to_auth("test".to_string(), "test".to_string()).unwrap();
+    assert_eq!(WebHookResult::Failed, auth_is_ok(&t))
+}
+
+#[test]
+fn test_check_auth_login_ok() {
+    let t = login_to_auth("88mph".to_string(), "88mph".to_string()).unwrap();
+    assert_eq!(WebHookResult::Ok, auth_is_ok(&t))
 }

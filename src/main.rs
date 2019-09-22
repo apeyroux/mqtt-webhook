e0b142f580c5400c55a7bcc029455d38df34769e
Alexandre Peyroux <alex@px.io>
2019-09-22 11:48:30 +0200

    add more config

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,15 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 use simplelog::*;
 use std::fs::File;
+use std::sync::Mutex;
 
 type IMEI = String;
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+struct Cfg {
+    url_neotoken: String,
+    mph_password: String,
+}
 
 #[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
 struct WebHookAuthPayload {
@@ -112,12 +119,14 @@ fn login_to_auth(username: String, password: String) -> Result<Authentication, S
     }
 }
 
-fn auth_is_ok(auth: &Authentication) -> Result<WebHookResult, WebHookResult> {
+fn auth_is_ok(auth: &Authentication, cfg: &Cfg) -> Result<WebHookResult, WebHookResult> {
     match auth {
         Authentication::Token { uid, token, .. } => {
             // todo: kk faire un param
+            // let request_url = req.url_for("neotoken", uid, token)?;
             let request_url = format!(
-                "http://neotoken.gendarmerie.fr/token/check/?uid={uid}&token={token}",
+                "{}?uid={uid}&token={token}",
+                baseurl = cfg.url_neotoken,
                 uid = uid,
                 token = token
             );
@@ -133,7 +142,7 @@ fn auth_is_ok(auth: &Authentication) -> Result<WebHookResult, WebHookResult> {
         }
         Authentication::Ident { .. } => Ok(WebHookResult::Ok),
         Authentication::Login { username, password }
-            if username == "88mph" && password == "88mph" =>
+            if username == "88mph" && password == &cfg.mph_password =>
         {
             Ok(WebHookResult::Ok)
         }
@@ -145,7 +154,11 @@ fn ws_auth_sub(_client: web::Json<WebHookAuthSubPayload>, _req: HttpRequest) -> 
     HttpResponse::Ok().json(WebHookResult::Ok)
 }
 
-fn ws_auth_pub(client: web::Json<WebHookAuthPubPayload>, req: HttpRequest) -> HttpResponse {
+fn ws_auth_pub(
+    cfg: web::Data<Mutex<Cfg>>,
+    client: web::Json<WebHookAuthPubPayload>,
+    req: HttpRequest,
+) -> HttpResponse {
     match req.headers().get("vernemq-hook") {
         Some(hv) if hv.to_str().unwrap() == "auth_on_publish" => match client.into_inner() {
             WebHookAuthPubPayload {
@@ -164,7 +177,11 @@ fn ws_auth_pub(client: web::Json<WebHookAuthPubPayload>, req: HttpRequest) -> Ht
     }
 }
 
-fn ws_auth(client: web::Json<WebHookAuthPayload>, req: HttpRequest) -> HttpResponse {
+fn ws_auth(
+    cfg: web::Data<Mutex<Cfg>>,
+    client: web::Json<WebHookAuthPayload>,
+    req: HttpRequest,
+) -> HttpResponse {
     match req.headers().get("vernemq-hook") {
         // auth_on_register
         Some(hv) if hv.to_str().unwrap() == "auth_on_register" => match client.into_inner() {
@@ -178,23 +195,24 @@ fn ws_auth(client: web::Json<WebHookAuthPayload>, req: HttpRequest) -> HttpRespo
                 clean_session: _,
             } => match login_to_auth(username, password) {
                 Ok(auth) => {
-                    if auth_is_ok(&auth) == Ok(WebHookResult::Ok) {
+                    let mut cfg = cfg.lock().unwrap();
+                    if auth_is_ok(&auth, &cfg) == Ok(WebHookResult::Ok) {
                         info!("ok {:?}", auth);
                         HttpResponse::Ok().json(WebHookResult::Ok)
                     } else {
                         info!("ko {:?}", auth);
-                        HttpResponse::Ok().json(WebHookResult::Failed)
+                        HttpResponse::Forbidden().json(WebHookResult::Failed)
                     }
                 }
                 Err(err) => {
                     info!("ko {}", err);
-                    HttpResponse::Ok().json(WebHookResult::Failed)
+                    HttpResponse::Forbidden().json(WebHookResult::Failed)
                 }
             },
-            _ => HttpResponse::Ok().json(WebHookResult::Failed),
+            _ => HttpResponse::Forbidden().json(WebHookResult::Failed),
         },
         // todo: mettre le auth_on_publish ici
-        _ => HttpResponse::Ok().json(WebHookResult::Failed), // <- send response
+        _ => HttpResponse::Forbidden().json(WebHookResult::Failed), // <- send response
     }
 }
 
@@ -214,16 +232,32 @@ fn main() {
                 .env("LISTEN_WS"),
         )
         .arg(
+            Arg::with_name("neotoken_url")
+                .long("neotoken-url")
+                .takes_value(true)
+                .default_value("http://neotoken.gendarmerie.fr/token/check/")
+                .env("MQTT_WEBHOOK_NEOTOKEN"),
+        )
+        .arg(
+            Arg::with_name("mph_password")
+                .long("mph-password")
+                .takes_value(true)
+                .default_value("88mph")
+                .env("MQTT_WEBHOOK_88MPH_PASSWORD"),
+        )
+        .arg(
             Arg::with_name("logfile")
                 .long("log")
                 .takes_value(true)
                 .default_value("mqtt-webhook.log")
-                .env("MQTT_WEBHOO_LOG"),
+                .env("MQTT_WEBHOOK_LOG"),
         )
         .get_matches();
 
     let listen = matches.value_of("listen").unwrap();
     let logfile = matches.value_of("logfile").unwrap();
+    let url_neotoken = matches.value_of("neotoken_url").unwrap();
+    let mph_password = matches.value_of("mph_password").unwrap();
 
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed).unwrap(),
@@ -235,8 +269,14 @@ fn main() {
     ])
     .unwrap();
 
-    let _ = HttpServer::new(|| {
+    let cfg = web::Data::new(Mutex::new(Cfg {
+        url_neotoken: url_neotoken.to_string(),
+        mph_password: mph_password.to_string(),
+    }));
+
+    let _ = HttpServer::new(move || {
         WebApp::new()
+            .register_data(cfg.clone())
             .data(web::JsonConfig::default().limit(4096))
             .service(web::resource("/auth").route(web::post().to(ws_auth)))
             .service(web::resource("/auth/pub").route(web::post().to(ws_auth_pub)))
